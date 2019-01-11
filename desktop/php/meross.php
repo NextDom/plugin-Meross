7f1ba65aba7df240430cfde769b63162b3cd31c8
DanoneKiD <simonj@hotmail.fr>
2019-01-11 16:24:05 +0100

    Informations eqLogic

--- desktop/php/meross.php
+++ desktop/php/meross.php
@@ -25,52 +25,48 @@ $eqLogics = eqLogic::byType($plugin->getId());
 
 ?>
 
-    <div class="row row-overflow">
-        <div class="col-lg-2 col-md-3 col-sm-4">
-            <div class="bs-sidebar">
-                <ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
-                    <li class="filter" style="margin-bottom: 5px;">
-                        <input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/>
-                    </li>
-                    <?php
+<div class="row row-overflow">
+    <div class="col-lg-2 col-md-3 col-sm-4">
+        <div class="bs-sidebar">
+            <ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
+                <li class="filter" style="margin-bottom: 5px;">
+                    <input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%" />
+                </li>
+                <?php
                     foreach ($eqLogics as $eqLogic) {
                         $opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
                         echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '" style="' . $opacity . '"><a>' . $eqLogic->getHumanName(true) . '</a></li>';
                     }
                     ?>
-                </ul>
+            </ul>
+        </div>
+    </div>
+
+    <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
+        <legend><i class="fa fa-cog"></i> {{Gestion}}</legend>
+        <div class="eqLogicThumbnailContainer">
+            <div class="cursor eqLogicAction" data-action="add" style="text-align: center; background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
+                <i class="fa fa-plus" style="font-size : 6em;color:#33b8cc;"></i>
+                <br>
+                <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;">{{ Ajouter }}</span>
+            </div>
+            <div class="cursor eqLogicAction" data-action="sync" style="text-align: center; background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
+                <i class="fa fa-sync" style="font-size : 6em;color:#33b8cc;"></i>
+                <br>
+                <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;">{{ Synchroniser }}</span>
+            </div>
+            <div class="cursor eqLogicAction" data-action="gotoPluginConf" style="text-align: center; background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
+                <i class="fa fa-wrench" style="font-size : 6em;color:#767676;"></i>
+                <br>
+                <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;">{{ Configuration }}</span>
             </div>
         </div>
 
-        <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay"
-             style="border-left: solid 1px #EEE; padding-left: 25px;">
-            <legend><i class="fa fa-cog"></i> {{Gestion}}</legend>
-            <div class="eqLogicThumbnailContainer">
-                <div class="cursor eqLogicAction" data-action="add"
-                     style="text-align: center; background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
-                    <i class="fa fa-plus" style="font-size : 6em;color:#33b8cc;"></i>
-                    <br>
-                    <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;">{{ Ajouter }}</span>
-                </div>
-                <div class="cursor eqLogicAction" data-action="sync"
-                     style="text-align: center; background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
-                    <i class="fa fa-sync" style="font-size : 6em;color:#33b8cc;"></i>
-                    <br>
-                    <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;">{{ Synchroniser }}</span>
-                </div>
-                <div class="cursor eqLogicAction" data-action="gotoPluginConf"
-                     style="text-align: center; background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
-                    <i class="fa fa-wrench" style="font-size : 6em;color:#767676;"></i>
-                    <br>
-                    <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;">{{ Configuration }}</span>
-                </div>
-            </div>
+        <br>
 
-            <br>
-
-            <legend><i class="fa fa-table"></i> {{Mes Meross}}</legend>
-            <div class="eqLogicThumbnailContainer">
-                <?php
+        <legend><i class="fa fa-table"></i> {{Mes Meross}}</legend>
+        <div class="eqLogicThumbnailContainer">
+            <?php
                 foreach ($eqLogics as $eqLogic) {
                     $opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
                     echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="text-align: center; background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
@@ -84,122 +80,135 @@ $eqLogics = eqLogic::byType($plugin->getId());
                     echo '</div>';
                 }
                 ?>
-            </div>
         </div>
+    </div>
 
-        <div class="col-lg-10 col-md-9 col-sm-8 eqLogic"
-             style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
-            <a class="btn btn-success eqLogicAction pull-right" data-action="save">
-                <i class="fa fa-check-circle"></i> {{Sauvegarder}}
-            </a>
-            <a class="btn btn-danger eqLogicAction pull-right" data-action="remove">
-                <i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
-            <a class="btn btn-default eqLogicAction pull-right" data-action="configure">
-                <i class="fa fa-cogs"></i> {{Configuration avancée}}
-            </a>
-            <ul class="nav nav-tabs" role="tablist">
-                <li role="presentation">
-                    <a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab"
-                       data-action="returnToThumbnailDisplay">
-                        <i class="fa fa-arrow-circle-left"></i>
-                    </a>
-                </li>
-                <li role="presentation" class="active">
-                    <a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab">
-                        <i class="fa fa-tachometer"></i> {{Equipement}}
-                    </a>
-                </li>
-                <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab">
-                        <i class="fa fa-list-alt"></i> {{Commandes}}</a>
-                </li>
-            </ul>
-            <div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
-                <div role="tabpanel" class="tab-pane active" id="eqlogictab">
-                    <br/>
-                    <form class="form-horizontal">
-                        <fieldset>
-                            <div class="form-group">
-                                <label class="col-sm-3 control-label" for="name">{{Nom de l'équipement
-                                    Meross}}</label>
-                                <div class="col-sm-3">
-                                    <input type="text" class="eqLogicAttr form-control" data-l1key="id"
-                                           style="display : none;"/>
-                                    <input type="text" class="eqLogicAttr form-control" data-l1key="name" id="name"
-                                           placeholder="{{Nom de l'équipement Meross}}"/>
-                                </div>
+    <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
+        <a class="btn btn-success eqLogicAction pull-right" data-action="save">
+            <i class="fa fa-check-circle"></i> {{Sauvegarder}}
+        </a>
+        <a class="btn btn-danger eqLogicAction pull-right" data-action="remove">
+            <i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+        <a class="btn btn-default eqLogicAction pull-right" data-action="configure">
+            <i class="fa fa-cogs"></i> {{Configuration avancée}}
+        </a>
+        <ul class="nav nav-tabs" role="tablist">
+            <li role="presentation">
+                <a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay">
+                    <i class="fa fa-arrow-circle-left"></i>
+                </a>
+            </li>
+            <li role="presentation" class="active">
+                <a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab">
+                    <i class="fa fa-tachometer"></i> {{Equipement}}
+                </a>
+            </li>
+            <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab">
+                    <i class="fa fa-list-alt"></i> {{Commandes}}</a>
+            </li>
+        </ul>
+        <div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
+            <div role="tabpanel" class="tab-pane active" id="eqlogictab">
+                <br />
+                <form class="form-horizontal">
+                    <fieldset>
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label" for="name">{{Nom de l'équipement
+                                Meross}}</label>
+                            <div class="col-sm-3">
+                                <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+                                <input type="text" class="eqLogicAttr form-control" data-l1key="name" id="name"
+                                    placeholder="{{Nom de l'équipement Meross}}" />
                             </div>
-                            <div class="form-group">
-                                <label class="col-sm-3 control-label" for="sel_object">{{Objet parent}}</label>
-                                <div class="col-sm-3">
-                                    <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
-                                        <option value="">{{Aucun}}</option>
-                                        <?php
+                        </div>
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label" for="sel_object">{{Objet parent}}</label>
+                            <div class="col-sm-3">
+                                <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
+                                    <option value="">{{Aucun}}</option>
+                                    <?php
                                         foreach (object::all() as $object) {
                                             echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
                                         }
                                         ?>
-                                    </select>
-                                </div>
+                                </select>
                             </div>
-                            <div class="form-group">
-                                <label class="col-sm-3 control-label">{{Catégorie}}</label>
-                                <div class="col-sm-9">
-                                    <?php
+                        </div>
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">{{Catégorie}}</label>
+                            <div class="col-sm-9">
+                                <?php
                                     foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
                                         echo '<label class="checkbox-inline" for="category-' . $key . '">';
                                         echo '<input type="checkbox" id="category-' . $key . '" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
                                         echo '</label>';
                                     }
                                     ?>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label"></label>
+                            <div class="col-sm-9">
+                                <label class="checkbox-inline" for="is-enable">
+                                    <input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked="checked"
+                                        id="is-enable" />
+                                    {{Activer}}
+                                </label>
+                                <label class="checkbox-inline" for="is-visible">
+                                    <input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked="checked"
+                                        id="is-visible" />
+                                    {{Visible}}
+                                </label>
+                            </div>
+                        </div>
+
+                        <!-- Informations -->
+                        <div class="row">
+                            <div class="col-md-4 col-md-offset-3">
+                                <div class="box box-widget widget-user-2">
+                                    <div class="widget-user-header bg-aqua-active">
+                                        <h3 class="widget-user-username">{{ Informations Meross Cloud }}</h3>
+                                    </div>
+                                    <div class="box-footer no-padding">
+                                        <ul class="nav nav-stacked">
+                                            <li><a href="#">{{ Modèle }}<span class="eqLogicAttr pull-right badge bg-blue" data-l1key="configuration" data-l2key="type"></span></a></li>
+                                            <li><a href="#">{{ Adresse IP }}<span class="eqLogicAttr pull-right badge bg-default" data-l1key="configuration" data-l2key="ip"></span></a></li>
+                                            <li><a href="#">{{ Adresse MAC }}<span class="eqLogicAttr pull-right badge bg-default" data-l1key="configuration" data-l2key="mac"></span></a></li>
+                                            <li><a href="#">{{ En ligne }}<span class="eqLogicAttr pull-right badge bg-green" data-l1key="configuration" data-l2key="online"></span></a></li>
+                                            <li><a href="#">{{ Nom sur l'app Meross }}<span class="eqLogicAttr pull-right badge bg-default" data-l1key="configuration" data-l2key="name"></span></a></li>
+                                            <li><a href="#">{{ UUID }}<span class="eqLogicAttr pull-right badge bg-default" data-l1key="configuration" data-l2key="uuid"></span></a></li>
+                                        </ul>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="form-group">
-                                <label class="col-sm-3 control-label"></label>
-                                <div class="col-sm-9">
-                                    <label class="checkbox-inline" for="is-enable">
-                                        <input type="checkbox" class="eqLogicAttr" data-l1key="isEnable"
-                                               checked="checked" id="is-enable"/>
-                                        {{Activer}}
-                                    </label>
-                                    <label class="checkbox-inline" for="is-visible">
-                                        <input type="checkbox" class="eqLogicAttr" data-l1key="isVisible"
-                                               checked="checked" id="is-visible"/>
-                                        {{Visible}}
-                                    </label>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label class="col-sm-3 control-label" for="Meross-param">{{ Modèle }}</label>
-                                <div class="col-sm-3">
-                                    <input type="text" class="eqLogicAttr form-control" id="type-param"
-                                           data-l1key="configuration" data-l2key="type" placeholder="{{Modèle}}"/>
-                                </div>
-                            </div>
-                        </fieldset>
-                    </form>
-                </div>
-                <div role="tabpanel" class="tab-pane" id="commandtab">
-                    <a class="btn btn-success btn-sm cmdAction pull-right" data-action="add" style="margin-top:5px;">
-                        <i class="fa fa-plus-circle"></i> {{Commandes}}
-                    </a>
-                    <br/>
-                    <br/>
-                    <table id="table_cmd" class="table table-bordered table-condensed">
-                        <thead>
+                        </div>
+                        <!-- End Informations -->
+
+                    </fieldset>
+                </form>
+            </div>
+            <div role="tabpanel" class="tab-pane" id="commandtab">
+                <a class="btn btn-success btn-sm cmdAction pull-right" data-action="add" style="margin-top:5px;">
+                    <i class="fa fa-plus-circle"></i> {{Commandes}}
+                </a>
+                <br />
+                <br />
+                <table id="table_cmd" class="table table-bordered table-condensed">
+                    <thead>
                         <tr>
                             <th>{{Nom}}</th>
                             <th>{{Type}}</th>
                             <th>{{Action}}</th>
                         </tr>
-                        </thead>
-                        <tbody>
-                        </tbody>
-                    </table>
-                </div>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
             </div>
-
         </div>
+
     </div>
+</div>
 
 <?php
 include_file('desktop', 'meross', 'js', 'meross');
