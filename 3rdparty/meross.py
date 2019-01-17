41701b6409e1677c04ae5845884fd51b95b45629
DanoneKiD <simonj@hotmail.fr>
2019-01-17 18:00:45 +0100

    Convert power in watt

--- 3rdparty/meross.py
+++ 3rdparty/meross.py
@@ -139,7 +139,7 @@ def RefreshOneDevice(device):
     # Current power
     try:
         electricity = device.get_electricity()
-        d['power'] = electricity['electricity']['power']
+        d['power'] = float(electricity['electricity']['power'] / 1000.)
     except:
         d['power'] = '-1'
 
